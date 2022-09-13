323321a120a1db4d6f5f6230b9a451ddcef74817
tfnLAB <tfnlab@gmail.com>
2022-09-12 22:32:44 -0700

    added burning meta

--- src/components/App.js
+++ src/components/App.js
@@ -247,7 +247,7 @@ offerPunkForSale = async (punkIndex, punkPrice) => {
 };
 claimPunk = async (punkIndex) => {
 
-  const price = window.web3.utils.toWei("0.005", "Ether");
+  const price = window.web3.utils.toWei("0.005", "Ether") * punkIndex;
   this.setState({ loading: true });
     this.state.cryptoBoysContract.methods
       .paidMint(punkIndex)
